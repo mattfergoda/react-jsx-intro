6d8d82563e69ce1f6b0e11213bf5af6935874782
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-10-30 12:12:52 -0700

    Fixing name slicing logic.

--- part-3/Person.js
+++ part-3/Person.js
@@ -4,7 +4,7 @@ function Person({ name, age, hobbies }) {
   return (
     <div>
       <p>Learn some information about this person.</p>
-      <p>Name: {name.slice(0,6)}</p>
+      <p>Name: {name.length > 8 ? name.slice(0,6): name}</p>
       <p>Hobbies:</p>
       <ul>
         {hobbies.map((h) => <li>{h}</li>)}
